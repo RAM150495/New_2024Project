1bc3a78272757d5a8720790baa1de6f7056b0258
ram150495 <ramaneeyansmecece2016@gmail.com>
2024-11-15 08:40:47 +0530

    new project

---
+++ src/test/java/org/test/Test.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Test {
+	private void testClass() {
+		System.out.println(" Print test case 1");
+
+	}
+	private void newTest() {
+		System.out.println(" Print test case 2");
+
+	}
+
+}
